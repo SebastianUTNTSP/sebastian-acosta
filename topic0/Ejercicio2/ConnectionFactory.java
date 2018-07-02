7e287aa57d684887f1599eea55ef7a14628d0dbf
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:41:45 -0300

    Create ConnectionFactory.java

---
+++ topic0/Ejercicio2/ConnectionFactory.java
@@ -0,0 +1,18 @@
+public class ConnectionFactory extends Abstractfactory {
+    @Override
+    Connection getConnection(String type) {
+        if (type.equalsIgnoreCase("oracle")) {
+            return new OracleDB();
+        }
+        if (type.equalsIgnoreCase("mssql")) {
+            return new Mssql();
+        }
+        return null;
+
+    }
+
+    @Override
+    Servers getServer(String type) {
+        return null;
+    }
+}
