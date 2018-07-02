4be7a1a2ef5863cd039e173365059ce8f5db557d
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:44:09 -0300

    Create MssqlServer.java

---
+++ topic0/Ejercicio2/MssqlServer.java
@@ -0,0 +1,16 @@
+public class MssqlServer implements Servers {
+    @Override
+    public String getUrl() {
+        return "MssqlUrl";
+    }
+
+    @Override
+    public String getUser() {
+        return "MssqlUser";
+    }
+
+    @Override
+    public String getPassword() {
+        return "password";
+    }
+}
