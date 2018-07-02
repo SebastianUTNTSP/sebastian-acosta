e19dc54a8540a5747f368410890ddc1eee726f28
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:45:20 -0300

    Create OracleServer.java

---
+++ topic0/Ejercicio2/OracleServer.java
@@ -0,0 +1,18 @@
+public class OracleServer implements Servers {
+
+
+    @Override
+    public String getUrl() {
+        return "OracleUrl";
+    }
+
+    @Override
+    public String getUser() {
+        return "OracleUser";
+    }
+
+    @Override
+    public String getPassword() {
+        return "qwerty";
+    }
+}
