35e8233bbfae266affe3d6a332ce4315629caaee
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:51:59 -0300

    Create ProxyCon.java

---
+++ topic0/Ejercicio3/ProxyCon.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+
+public class ProxyCon implements Databaseinfo {
+    private RealCon realCon;
+    private String url;
+
+    public ProxyCon(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public Connection getConnection() {
+        if(realCon == null){
+            realCon = new RealCon(url);
+            return realCon.getConnection();
+        } else
+            return realCon.getConnection();
+
+
+    }
+}
