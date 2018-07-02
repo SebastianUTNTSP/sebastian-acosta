00831fd930c4dd9ee65ede59f458a4279436c6d0
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:42:25 -0300

    Create FactoryProducer.java

---
+++ topic0/Ejercicio2/FactoryProducer.java
@@ -0,0 +1,11 @@
+public class FactoryProducer {
+    public static Abstractfactory getFactory(String type) {
+        if (type.equalsIgnoreCase("connection")) {
+            return new ConnectionFactory();
+        }
+        if (type.equalsIgnoreCase("server")) {
+            return new ServerFactory();
+        }
+        return null;
+    }
+}
