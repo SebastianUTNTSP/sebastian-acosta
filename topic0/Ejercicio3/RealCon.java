f8e1996effbe1f5cd5fe4121919441425c6d8f24
SebastianUTN-TSP <as96acosta@gmail.com>
2018-07-02 16:53:43 -0300

    Create RealCon.java

---
+++ topic0/Ejercicio3/RealCon.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+
+public class RealCon implements Databaseinfo {
+    private Connection conn;
+    private String url;
+    public RealCon(String url) {
+        this.url = url;
+
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public Connection getConnection() {
+        System.out.println(getUrl());
+        return conn;
+    }
+}
